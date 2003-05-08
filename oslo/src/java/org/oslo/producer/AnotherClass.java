8e7ea46340462807504bf7589b493450e630e48a
amor <amor@fe7b895b-b372-0410-8246-c73cd64e3d3d>
2003-05-08 16:47:08 +0000

    This commit was generated by cvs2svn to compensate for changes in r4, which included commits to RCS files with non-trunk default branches.

---
+++ oslo/src/java/org/oslo/producer/AnotherClass.java
@@ -0,0 +1,14 @@
+package org.oslo.producer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ac08683
+ * Date: 30.apr.2003
+ * Time: 10:20:56
+ * To change this template use Options | File Templates.
+ */
+public class AnotherClass {
+  public void doLog1() {
+
+  }
+}
