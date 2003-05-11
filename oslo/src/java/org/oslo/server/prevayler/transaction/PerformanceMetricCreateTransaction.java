8c9f38cbbea3a2d5dc445655c31641153dbb9172
amor <amor@fe7b895b-b372-0410-8246-c73cd64e3d3d>
2003-05-11 21:13:13 +0000

    Introduced loggin using Prevayler, only testcode no real metrics

--- oslo/src/java/org/oslo/server/prevayler/transaction/PerformanceMetricCreateTransaction.java
+++ oslo/src/java/org/oslo/server/prevayler/transaction/PerformanceMetricCreateTransaction.java
@@ -1,6 +1,8 @@
 package org.oslo.server.prevayler.transaction;
 
 import org.prevayler.util.TransactionWithQuery;
+import org.oslo.server.infounits.PerformanceMetric;
+import org.oslo.server.prevayler.system.RantSystem;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,8 +12,35 @@ import org.prevayler.util.TransactionWithQuery;
  * To change this template use Options | File Templates.
  */
 public class PerformanceMetricCreateTransaction extends TransactionWithQuery {
+    private int _id;
+    private String _className;
+    private String _methodeName;
+    private String _ip;
+    private long _measurementDateMSec;
+    private long _innTimeMSec;
+    private long _outTimeMSec;
 
-    protected Object executeAndQuery(Object o) throws Exception {
-        return null;
+    public PerformanceMetricCreateTransaction(int _id, String _className, String _methodeName, String _ip, long _measurementDateMSec, long _innTimeMSec, long _outTimeMSec) {
+        this._id = _id;
+        this._className = _className;
+        this._methodeName = _methodeName;
+        this._ip = _ip;
+        this._measurementDateMSec = _measurementDateMSec;
+        this._innTimeMSec = _innTimeMSec;
+        this._outTimeMSec = _outTimeMSec;
+    }
+
+    protected Object executeAndQuery(Object system) throws Exception {
+        PerformanceMetric performanceMetric = null;
+        RantSystem rantSystem = (RantSystem)system;
+
+        if(!rantSystem.checkCreatePerformanceMetric(_id, _className, _methodeName, _ip, _measurementDateMSec, _innTimeMSec, _outTimeMSec)) {
+            throw new IllegalArgumentException("Invalid performance metric parameters");
+        }
+
+        performanceMetric = new PerformanceMetric(_id, _className, _methodeName, _ip, _measurementDateMSec, _innTimeMSec, _outTimeMSec);
+        rantSystem.addPerformanceMetric(performanceMetric);
+
+        return performanceMetric;
     }
 }
