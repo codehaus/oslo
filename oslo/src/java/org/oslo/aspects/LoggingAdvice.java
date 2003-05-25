4a2d4dde563b94ebadf8812305c799f89c24c5bb
amor <amor@fe7b895b-b372-0410-8246-c73cd64e3d3d>
2003-05-25 21:56:26 +0000

    commiting some new added functionality

--- oslo/src/java/org/oslo/aspects/LoggingAdvice.java
+++ oslo/src/java/org/oslo/aspects/LoggingAdvice.java
@@ -1,6 +1,7 @@
 package org.oslo.aspects;
 
 import org.oslo.producer.MetricLogger;
+import org.oslo.aspects.introduction.RantProcessIDIntroduction;
 import org.codehaus.aspectwerkz.joinpoint.JoinPoint;
 import org.codehaus.aspectwerkz.joinpoint.MethodJoinPoint;
 import org.codehaus.aspectwerkz.advice.AroundAdvice;
@@ -13,19 +14,40 @@ import org.codehaus.aspectwerkz.advice.AroundAdvice;
  * To change this template use Options | File Templates.
  */
 public class LoggingAdvice extends AroundAdvice {
-  public LoggingAdvice() {
-    super();
-  }
+    public LoggingAdvice() {
+        super();
+    }
 
-  public Object execute(JoinPoint joinPoint) throws Throwable {
-    MethodJoinPoint jp = (MethodJoinPoint)joinPoint;
+    public Object execute(JoinPoint joinPoint) throws Throwable {
+        MethodJoinPoint jp = (MethodJoinPoint) joinPoint;
 
-    MetricLogger dataProducer = MetricLogger.getInstance();
-    //dataProducer.logJointPointEntry(jp);
+        //MetricLogger dataProducer = MetricLogger.getInstance();
+        ///dataProducer.logJointPointEntry(jp);
+        System.out.println("Hello");
 
-    final Object result = joinPoint.proceed();
+        Object[] objects = jp.getParameters();
 
-    //dataProducer.logJointPointExit(jp);
-    return result;
-  }
+        for (int i = 0; i < objects.length; i++) {
+            Object object = objects[i];
+
+            // Check if this contains the actual Interface
+            if(object instanceof RantProcessIDIntroduction)
+                System.out.println("++ Enter Object: " + object.toString());
+            else
+                System.out.println("Enter Object: " + object.toString());
+        }
+
+        Class[] classes = jp.getParameterTypes();
+
+        for (int i = 0; i < classes.length; i++) {
+            Class aClass = classes[i];
+            System.out.println("Class name: " + aClass.getName());
+        }
+
+        // Execute the joinpoint
+        final Object result = joinPoint.proceed();
+
+        //dataProducer.logJointPointExit(jp);
+        return result;
+    }
 }
--- oslo/src/java/org/oslo/sample/SampleLevel1.java
+++ oslo/src/java/org/oslo/sample/SampleLevel1.java
@@ -8,14 +8,18 @@ package org.oslo.sample;
  * To change this template use Options | File Templates.
  */
 public class SampleLevel1 {
-  SampleLevel2 sampleLevel2 = new SampleLevel2();
+    SampleLevel2 sampleLevel2 = new SampleLevel2();
 
-  public SampleLevel1() {
-  }
+    public SampleLevel1() {
+    }
 
-  public boolean doSomethingLevel1() {
-    int number = sampleLevel2.doSomethingLevel2nr1();
-    Object object = sampleLevel2.doSomethingLevel2nr2();
-    return true;
-  }
+    public boolean doSomethingLevel1() {
+        int number = sampleLevel2.doSomethingLevel2nr1();
+        Object object = sampleLevel2.doSomethingLevel2nr2();
+        return true;
+    }
+
+    public void doSomethingLevel2_nr2(String variable, int number, SampleLevel2 samplelevel2) {
+        return;
+    }
 }
